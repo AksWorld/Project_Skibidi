835932a516461cfad7d9c0e6f498bd7f6e2ebf35
AksWorld <123278691+AksWorld@users.noreply.github.com>
2023-09-26 22:31:55 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.background(200,255,255);
+    canvas.center();
+}
+function draw(){
+}
+function Operation_G_Man(){
+    save("Scientist_Toilet_filters.png");
+}
